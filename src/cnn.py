40a5ceb8f9c768ecb17879e3ec888abe5c8476d7
xinxiang wang <angelowxx@gmail.com>
2024-12-12 22:56:37 +0000

    updating from colab

--- src/cnn.py
+++ src/cnn.py
@@ -52,20 +52,20 @@ class HomemadeModel(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         x = self.pool(x)
-        x = self.dropout2d(x)
+        # x = self.dropout2d(x)
         x = self.conv2(x)
         x = self.pool(x)
         x = self.dropout2d(x)
         x = self.conv3(x)
         x = self.pool(x)
-        x = self.dropout2d(x)
+        # x = self.dropout2d(x)
         x = self.conv4(x)
         x = self.pool(x)
 
 
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
-        x = self.dropout(x)
+        # x = self.dropout(x)
         x = F.leaky_relu(x)
         x = self.fc2(x)
 
