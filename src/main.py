c94adefff377ba5168494b2aced5db63cc4ee22c
Xinxiang <angelowxx@gmail.com>
2024-12-15 00:13:12 +0100

    post training single layer

--- src/main.py
+++ src/main.py
@@ -126,8 +126,7 @@ def main(data_dir,
         info = 'Training single layer [{}/{}]'.format(i+1, 10)
         model.step()
         learning_rate = 0.0005
-        model.freeze_convolution_layers()
-        train_model(save_model_str, 20, model, learning_rate
+        train_model(save_model_str, 10, model, learning_rate
                     , train_criterion, train_loader, device, model_optimizer
                     , use_all_data_to_train, val_loader, exp_name, score, info)
 
