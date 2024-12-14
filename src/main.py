f9cac84b65e6137abaebe656d140ee96ff412200
Xinxiang <angelowxx@gmail.com>
2024-12-14 22:18:22 +0100

    Revert "0.8"
    This reverts commit 7bdd59f2

--- src/main.py
+++ src/main.py
@@ -95,27 +95,54 @@ def main(data_dir,
     if continue_training:
         model.load_state_dict(torch.load(os.path.join(os.getcwd(), 'models', 'default_model')))
     else:
-        train_model(save_model_str, 30, model, 0.005
+        train_model(save_model_str, 30, model, 0.003
                     , train_criterion, train_loader, device, model_optimizer
-                    , use_all_data_to_train, val_loader, exp_name, score, 'Pre-training')
+                    , use_all_data_to_train, val_loader, exp_name, score, 'Pretraining')
 
-    data_augmentation = data_augmentation_pipline
+    data_augmentations = [cropping_img, translation_rotation, data_augmentation_pipline]
+    augmentation_times = [2, 2, 3]
+    num_epochs = [20, 20, 50]
+    learning_rates = [0.005, 0.002, 0.002]
 
-
-    num_loops = 3
-    learning_rate = 0.001
-    for i in range(num_loops):
-        info = 'Post-training [{}/{}]'.format(i+1, num_loops)
+    augmentation_types = len(data_augmentations)
+    train_data = [train_data]
+    # model.add_dropout()
+    for i in range(augmentation_types):
+        data_augmentation = data_augmentations[i]
+        augmentation_time = augmentation_times[i]
+        info = 'Data augmentation [{}/{}]'.format(i + 1, augmentation_types)
         train_data = [ImageFolder(os.path.join(data_dir, 'train'), transform=data_augmentation) for i in
-                      range(2)]
+                      range(augmentation_time)] + train_data
+
         train_loader = DataLoader(dataset=ConcatDataset(train_data),
                                   batch_size=batch_size,
                                   shuffle=True)
-        #model.freeze_linear_layers()
-        train_model(save_model_str, 30, model, learning_rate
+        train_model(save_model_str, num_epochs[i], model, learning_rates[i]
                     , train_criterion, train_loader, device, model_optimizer
                     , use_all_data_to_train, val_loader, exp_name, score, info)
-        learning_rate /= 2
+
+    train_loader = DataLoader(dataset=ConcatDataset(train_data),
+                              batch_size=batch_size,
+                              shuffle=True)
+
+    info = 'Fine tuning [1/3]'
+    learning_rate = 0.0005
+    model.freeze_convolution_layers()
+    train_model(save_model_str, 20, model, learning_rate
+                , train_criterion, train_loader, device, model_optimizer
+                , use_all_data_to_train, val_loader, exp_name, score, info)
+    info = 'Fine tuning [2/3]'
+    learning_rate = 0.00001
+    model.freeze_linear_layers()
+    train_model(save_model_str, 20, model, learning_rate
+                , train_criterion, train_loader, device, model_optimizer
+                , use_all_data_to_train, val_loader, exp_name, score, info)
+    learning_rate = 0.00001
+    info = 'Fine tuning [3/3]'
+    model.freeze_convolution_layers()
+    train_model(save_model_str, 20, model, learning_rate
+                , train_criterion, train_loader, device, model_optimizer
+                , use_all_data_to_train, val_loader, exp_name, score, info)
 
     if not use_all_data_to_train:
         logging.info('Accuracy at each epoch: ' + str(score))
