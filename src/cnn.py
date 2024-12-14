b29e1d8e0d8ad2f40615657c67a006a3fe13ff3b
Xinxiang <angelowxx@gmail.com>
2024-12-14 23:07:07 +0100

    update

--- src/cnn.py
+++ src/cnn.py
@@ -93,17 +93,20 @@ class HandmadeModel(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=input_shape[0], out_channels=20, kernel_size=(3, 3), padding=(1, 1))
 
         self.conv2 = nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(3, 3), padding=(1, 1))
-        self.conv3 = nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(5, 5), padding=(2, 2))
-        self.conv4 = nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(7, 7), padding=(3, 3))
+        self.conv3 = nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(3, 3), padding=(2, 2), dilation=2)
+        self.conv4 = nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(3, 3), padding=(3, 3), dilation=3)
 
-        self.conv5 = nn.Conv2d(in_channels=30, out_channels=60, kernel_size=(3, 3), padding=(1, 1))
-        self.conv6 = nn.Conv2d(in_channels=60, out_channels=60, kernel_size=(3, 3), padding=(1, 1))
-        self.conv7 = nn.Conv2d(in_channels=60, out_channels=70, kernel_size=(3, 3), padding=(1, 1))
+        self.conv5 = nn.Conv2d(in_channels=30, out_channels=15, kernel_size=(3, 3), padding=(1, 1))
+        self.conv6 = nn.Conv2d(in_channels=30, out_channels=15, kernel_size=(3, 3), padding=(2, 2), dilation=2)
+        self.conv7 = nn.Conv2d(in_channels=30, out_channels=15, kernel_size=(3, 3), padding=(3, 3), dilation=3)
+
+        self.conv8 = nn.Conv2d(in_channels=45, out_channels=65, kernel_size=(3, 3), padding=(1, 1))
+        self.conv9 = nn.Conv2d(in_channels=65, out_channels=90, kernel_size=(3, 3), padding=(1, 1))
         self.pool = nn.MaxPool2d(3, stride=2)
         # The input features for the linear layer depends on the size of the input to the convolutional layer
         # So if you resize your image in data augmentations, you'll have to tweak this too.
         #self.fc1 = nn.Linear(in_features=60, out_features=30)
-        self.fc2 = nn.Linear(in_features=70, out_features=num_classes)
+        self.fc2 = nn.Linear(in_features=90, out_features=num_classes)
 
         self.dropout = nn.Dropout(p=0.2)
         self.dropout2d = nn.Dropout2d(p=0.2)
@@ -119,16 +122,18 @@ class HandmadeModel(nn.Module):
 
         x = torch.cat((x1, x2, x3), dim=1)
         x = self.pool(x)
-        #x = self.dropout2d(x)
 
-        x = self.conv5(x)
+        x1 = self.conv5(x)
+        x2 = self.conv6(x)
+        x3 = self.conv7(x)
+
+        x = torch.cat((x1, x2, x3), dim=1)
         x = self.pool(x)
-        #x = self.dropout2d(x)
-        x = self.conv6(x)
+
+
+        x = self.conv8(x)
         x = self.pool(x)
-        #x = self.dropout2d(x)
-        x = self.conv7(x)
-        #x = torch.cat((x, x1), dim=1)
+        x = self.conv9(x)
         x = self.pool(x)
 
         x = x.view(x.size(0), -1)
