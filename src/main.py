be1ee5b9213e0efda9016c433feb039fc94ad87a
Xinxiang <angelowxx@gmail.com>
2024-12-15 00:15:28 +0100

    post training single layer

--- src/main.py
+++ src/main.py
@@ -125,7 +125,7 @@ def main(data_dir,
     for i in range(20):
         info = 'Training single layer [{}/{}]'.format(i+1, 10)
         model.step()
-        learning_rate = 0.0005
+        learning_rate = 0.0003
         train_model(save_model_str, 6, model, learning_rate
                     , train_criterion, train_loader, device, model_optimizer
                     , use_all_data_to_train, val_loader, exp_name, score, info)
