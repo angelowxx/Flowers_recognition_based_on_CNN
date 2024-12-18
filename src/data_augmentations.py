157ea27d3bdffe948300a0820c53aa3f7986eb8a
Xinxiang <angelowxx@gmail.com>
2024-12-18 15:02:16 +0100

    add kaggle dataset

--- src/data_augmentations.py
+++ src/data_augmentations.py
@@ -60,11 +60,12 @@ cropping_img = transforms.Compose([
 ])
 
 data_augmentation_pipline = transforms.Compose([
-    transforms.RandomCrop(size=(128, 128)),
+    transforms.RandomCrop(size=(192, 192)),
     transforms.RandomAffine(degrees=(0, 90), translate=(0.3, 0.3), shear=30, scale=(0.7, 1.3)),  #
     transforms.ColorJitter(brightness=0.5, contrast=0.3, saturation=0.3, hue=0.1),
     transforms.RandomVerticalFlip(p=0.5),
     transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomCrop(size=(128, 128)),
     transforms.ToTensor(),
 ])
 
